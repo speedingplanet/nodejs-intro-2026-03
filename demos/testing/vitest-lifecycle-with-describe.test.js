3eae4ee58f6e4249450274d43f6bcb28be8fa134
John Paxton <johnpaxton@gmail.com>
2026-03-24 16:16:34 -0400

    Lifecycle and describe

---
+++ demos/testing/vitest-lifecycle-with-describe.test.js
@@ -0,0 +1,27 @@
+import { test, expect, describe, beforeEach, beforeAll, afterEach, afterAll } from 'vitest';
+
+// How many times will this run? (4 times)
+beforeEach(() => console.log('Outer beforeEach'));
+
+describe('First two tests', () => {
+	// How many times will this run? (2 times)
+	beforeEach(() => console.log('Inner beforeEach'));
+	test('Test 1', () => {
+		expect(1).toBeTruthy();
+	});
+	test('Test 2', () => {
+		expect(2).toBeTruthy();
+	});
+});
+
+describe('Last two tests', () => {
+	test('Test 3', () => {
+		expect(3).toBeTruthy();
+	});
+	test('Test 4', () => {
+		expect(4).toBeTruthy();
+	});
+});
+
+// How many times will this run? (Once)
+beforeAll(() => console.log('Outer beforeAll'));
