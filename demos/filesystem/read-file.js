f5887b5ad88efd896a2a967566b8a4d3b2a0ef01
John Paxton <johnpaxton@gmail.com>
2026-03-25 09:12:46 -0400

    Day three

---
+++ demos/filesystem/read-file.js
@@ -0,0 +1,73 @@
+// Read ../../data/users.csv
+import fs from 'node:fs';
+import fsPromises from 'node:fs/promises';
+import path from 'node:path';
+import chalk from 'chalk';
+
+// Directory path to this file
+let __dirname = import.meta.dirname;
+let goodPath = path.resolve(__dirname, '../../data/users.csv');
+let badPath = path.resolve(__dirname, '../../data/nonexistent-data.csv');
+
+// Old school: use callbacks
+// Check for access permissions
+// F_OK indicates a visible file, not necessarily any other RWX permissions
+
+function accessFile(filePath) {
+	fs.access(filePath, fs.constants.F_OK, (err) => {
+		if (err) {
+			console.error(chalk.red(`fs.access: Could not access file at ${filePath}`));
+		} else {
+			console.log(chalk.blue(`fs.access: File ${filePath} exists and is visible.`));
+		}
+	});
+}
+
+accessFile(goodPath);
+accessFile(badPath);
+
+// Promises API
+function accessFileWithPromises(filePath) {
+	// promise.then(successCallback, failureCallback)
+	/*
+	fsPromises.access(filePath, fs.constants.F_OK).then(
+		() => {
+			console.log(chalk.blue(`fs-promises.access: File ${filePath} exists and is visible.`));
+		},
+		() => {
+			console.error(chalk.red(`fs-promises.access: Could not access file at ${filePath}`));
+		}
+	);
+	*/
+
+	// Alternative style: promise.then(successCallback).catch(errorCallback)
+	fsPromises
+		.access(filePath, fs.constants.F_OK)
+		.then(() => {
+			console.log(chalk.blue(`fs-promises.access: File ${filePath} exists and is visible.`));
+		})
+		.catch(() => {
+			console.error(chalk.red(`fs-promises.access: Could not access file at ${filePath}`));
+		});
+}
+
+accessFileWithPromises(goodPath);
+accessFileWithPromises(badPath);
+
+// async-await
+async function accessFileWithAsyncAwait(filePath) {
+	try {
+		let accessResult = await fsPromises.access(filePath, fs.constants.F_OK);
+		// access() returns `undefined` on success. That's a choice.
+		if (!accessResult) {
+			console.log(chalk.blue(`async/await: File ${filePath} exists and is visible.`));
+		}
+	} catch (error) {
+		console.error(
+			chalk.red(`async/await: Could not access file at ${filePath} because ${error.message}`)
+		);
+	}
+}
+
+accessFileWithAsyncAwait(goodPath);
+accessFileWithAsyncAwait(badPath);
---
+++ demos/testing/vitest-skip-only.test.js
@@ -0,0 +1,27 @@
+import { test, expect, describe } from 'vitest';
+
+describe('Skip/only, group 1', () => {
+	test('Group 1, test 1', () => {
+		expect(1).toBeTruthy();
+	});
+
+	test.todo('Group 1, test 2', () => {});
+
+	test.skip('Group 1, test 3', () => {
+		expect(1).toBeTruthy();
+	});
+});
+
+describe.skip('Skip/only, group 2', () => {
+	test('Group 2, test 1', () => {
+		expect(1).toBeTruthy();
+	});
+
+	test('Group 2, test 2', () => {
+		expect(1).toBeTruthy();
+	});
+
+	test('Group 2, test 3', () => {
+		expect(1).toBeTruthy();
+	});
+});
